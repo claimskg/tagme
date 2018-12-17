ca09e67b2b26e53700b6b9b78e0ed603d13e2e87
Andon Tchechmedjiev <twk.theainur@gmail.com>
2018-12-17 13:04:24 +0100

    Fixes in json escaping and formatting for proper inclusion into the CSV output

--- src/org/gesis/AnnotateDataset.java
+++ src/org/gesis/AnnotateDataset.java
@@ -4,7 +4,6 @@ import it.acubelab.tagme.*;
 import it.acubelab.tagme.config.TagmeConfig;
 import it.acubelab.tagme.preprocessing.TopicSearcher;
 import me.tongfei.progressbar.ProgressBar;
-import org.apache.commons.csv.CSVFormat;
 
 import java.io.File;
 import java.io.IOException;
@@ -48,7 +47,7 @@ public enum AnnotateDataset {
                 .filter(filter).collect(Collectors.toList());
 
 
-        return AnnotateDataset.annotationsToJson(filteredAnnotations, ann_text, lang);
+        return AnnotateDataset.annotationsToEscapedJson(filteredAnnotations, ann_text, lang);
 
     }
 
@@ -82,11 +81,16 @@ public enum AnnotateDataset {
     }
 
     @SuppressWarnings({"LocalVariableOfConcreteClass", "MethodParameterOfConcreteClass", "FeatureEnvy"})
-    private static String annotationsToJson(final Iterable<? extends Annotation> annotations, AnnotatedText ann_text, String lang) throws IOException {
+    private static String annotationsToEscapedJson(final Iterable<? extends Annotation> annotations, AnnotatedText ann_text, String lang) throws IOException {
         TopicSearcher searcher = new TopicSearcher(lang);
         StringBuilder jsonBuilder = new StringBuilder();
-
+        boolean first = true;
         for (Annotation annotation : annotations) {
+            if (first) {
+                first = false;
+            } else {
+                jsonBuilder.append(",");
+            }
 
             int wikipediaId = annotation.getTopic();
             int begin = annotation.getStart();
@@ -103,14 +107,8 @@ public enum AnnotateDataset {
                 categories = new String[0];
             }
 
-            jsonBuilder.append(String.format("{\n" +
-                            "\t\"id\" : %d,\n " +
-                            "\t\"begin\": %d,\n" +
-                            "\t\"end\": %d,\n" +
-                            "\t\"entity\": \"%s\",\n" +
-                            "\t\"text\": \"%s\" ,\n" +
-                            "\t\"score\": %.2f, \n" +
-                            "\t\"categories\" : [%s]\n}\n",
+            jsonBuilder.append(String.format("{\"id\" : %d\",\"\"begin\": %d,\"end\": %d,\"entity\": \"%s\"," +
+                            "\"text\": \"%s\",\"score\": %.2f,\"categories\" : [%s]}",
                     wikipediaId,
                     begin,
                     end,
